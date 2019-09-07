47656918e144d48d940db57eed3d216aef4ad904
Наталия <@LAPTOP-F6D3T255>
2019-09-07 20:19:07 +0300

    classes added for GroupCreationTests

---
+++ addressbook-web-tests/addressbook-web-tests/Figure.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+
+{
+    class Figure
+    {
+        private bool colored = false;
+
+        public bool Colored
+        {
+            get
+            {
+                return colored;
+            }
+            set
+            {
+                colored = value;
+            }
+        }
+    }
+}
---
+++ addressbook-web-tests/addressbook-web-tests/Square.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WebAddressbookTests
+
+{
+    class Square : Figure
+    {
+        private int size;
+
+        public Square(int size)
+        {
+            this.size = size;
+        }
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+            }
+        }
+    }
+}
